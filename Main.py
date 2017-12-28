a447fdbb00538deabf15aac0ca87a2f21f9168fc
Landen Brown <landensbrown@gmail.com>
2017-12-28 12:29:48 -0800

    Menu Preview (Finished)
    The menu now has 100% functionality based on needed purposes. Will focus on developing other objects.

--- Main.py
+++ Main.py
@@ -1,6 +1,7 @@
 import time, math, os, easygui
 from easygui import *
 from Assets import *
+enginecheck = 1.5
 
 def mainEngine():
     easygui.msgbox("You have started the main engine!")
@@ -16,18 +17,23 @@ def viewCharacter():
         easygui.msgbox("You have made a selection!")
         if option == "Back":
             charchoice = "Back"
+def newCharacter():
+    easygui.msgbox("Here you will be able to create a new character!");
         
 
-choice = buttonbox("Welcome to TUDC!", "v0.01",
-                   ["Play Game", "Load Game", "View Character", "New Character", "Quit"])
-if choice == "Quit":
-    exit()
-if choice == "Play Game":
-    mainEngine()
-if choice == "Load Game":
-    loadGame()
-if choice == "View Character":
-    viewCharacter()
+while enginecheck != "Quit":
+    choice = buttonbox("Welcome to TUDC!", "v0.01",
+                       ["Play Game", "Load Game", "View Character", "New Character", "Quit"])
+    if choice == "Quit":
+        enginecheck = "Quit"
+    if choice == "Play Game":
+        mainEngine()
+    if choice == "Load Game":
+        loadGame()
+    if choice == "View Character":
+        viewCharacter()
+    if choice == "New Character":
+        newCharacter()
 
 
                     
