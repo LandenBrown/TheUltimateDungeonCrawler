1c88441dbd37b5dd8e51441e885bd63721d25e87
Landen Brown <landensbrown@gmail.com>
2017-12-28 12:58:11 -0800

    Changes

--- Assets/MonsterType.py
+++ Assets/MonsterType.py
@@ -7,9 +7,16 @@ class MonsterType:
         self.damagebonus = damagebonus
         self.weakness = weakness
         self.strength = strength
+        self.attack1 = attack1
+        self.attack2 = attack2
+        self.attack3 = attack3
+        self.attack4 = attack4
+        self.attack5 = attack5
         
-    def attack():
-        self.monstertype.attack()
+
+earthen = MonsterType("Earthen", 0, None, None)
+void = MonsterType("Void Realm", 5, None, None)
+chaotic = MonsterType("Chaotic", 7, None, None)
 
 
 
