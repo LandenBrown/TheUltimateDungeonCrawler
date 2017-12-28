8637338a287e701420eabab72bb8ca9610554992
Landen Brown <landensbrown@gmail.com>
2017-12-28 12:21:48 -0800

    Developed a bit of the main engine
    Just added the starting menu and a preview of the functionality therein.

--- Main.py
+++ Main.py
@@ -1,5 +1,33 @@
 import time, math, os, easygui
+from easygui import *
 from Assets import *
 
+def mainEngine():
+    easygui.msgbox("You have started the main engine!")
+def loadGame():
+    easygui.msgbox("This is where you will select a save!")
+    mainEngine()
+def viewCharacter():
+    easygui.msgbox("Here you will select your characters!")
+    charchoice = None
+    while charchoice != "Back":
+        option = easygui.buttonbox("Please select a character!", "v0.01",
+                                       ["Character 1", "Character 2", "Character 3", "Back"])
+        easygui.msgbox("You have made a selection!")
+        if option == "Back":
+            charchoice = "Back"
+        
 
-easygui.msgbox("Hello stupid fucking world")
+choice = buttonbox("Welcome to TUDC!", "v0.01",
+                   ["Play Game", "Load Game", "View Character", "New Character", "Quit"])
+if choice == "Quit":
+    exit()
+if choice == "Play Game":
+    mainEngine()
+if choice == "Load Game":
+    loadGame()
+if choice == "View Character":
+    viewCharacter()
+
+
+                    
