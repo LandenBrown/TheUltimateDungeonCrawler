f8963f11484a9c369dec3e67e1ffd9943f0a317f
Landen Brown <landensbrown@gmail.com>
2017-12-29 12:56:59 -0800

    Armor class

---
+++ Assets/Armor.py
@@ -0,0 +1,14 @@
+from __init__ import *
+
+class Armor:
+        def __init__(self, name, hpbonus, cost, enhancement):
+                self.name = name
+                self.hpbonus = hpbonus
+                self.cost = cost
+                self.enhancement = enhancement
+	
+
+
+
+			
+
