8465f0868bcad5351f63771a9ad6085a65f2add5
calebeasley <34135625+calebeasley@users.noreply.github.com>
2017-12-29 13:34:45 -0700

    Changed text

--- Assets/Weapon_Enhancement.py
+++ Assets/Weapon_Enhancement.py
@@ -1,5 +1,6 @@
 from __init__ import *
 
+# I decided part way though this that we will want both weapon and armor enhancement's.
 class Weapon_Enhancement:
 	def __init__(self, name, damage_modifier, elemental_type, swing_speed_modifier)
 		self.name = name
