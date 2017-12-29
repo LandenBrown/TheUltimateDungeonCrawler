c3a715db134f9ff0af39f26d5c118258de5155e8
calebeasley <34135625+calebeasley@users.noreply.github.com>
2017-12-29 13:33:03 -0700

    Created Weapon Enhancement class

---
+++ Assets/Weapon_Enhancement.py
@@ -0,0 +1,9 @@
+from __init__ import *
+
+class Weapon_Enhancement:
+	def __init__(self, name, damage_modifier, elemental_type, swing_speed_modifier)
+		self.name = name
+		self.damage_modifier = damage_modifier
+		self.elemental_type = elemental_type
+		self.swing_speed_modifier = swing_speed_modifier
+		
