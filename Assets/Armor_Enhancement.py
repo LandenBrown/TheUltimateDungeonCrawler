f9760068ace499598f7649f8a1d48ee0cea241bb
calebeasley <34135625+calebeasley@users.noreply.github.com>
2017-12-29 13:41:10 -0700

    Created Armor Enhancement asset

---
+++ Assets/Armor_Enhancement.py
@@ -0,0 +1,8 @@
+from __init__ import *
+
+class Armor_Enhancement:
+	def __init__(self, name, hp_bonus, damage_resistance, player_speed_modifier)
+		self.name = name
+		self.hp_bonus = hp_bonus
+		self.damage_resistance = damage_resistance
+		self.player_speed_modifier = player_speed_modifier
