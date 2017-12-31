8b5594cf7e9d92e9ca7607166040ac163a19d1c8
Landen Brown <31902019+LandenBrown@users.noreply.github.com>
2017-12-31 13:30:15 +0300

    Construct Changes

--- Assets/Weapon.py
+++ Assets/Weapon.py
@@ -1,39 +1,32 @@
 from __init__ import *
 
 class Weapon:
-	def __init__(self, name, damage, swing_speed, level, durability, max_durability, max_damage_buffer, swing_buffer, cost):
+	def __init__(self, name, damage, swing_speed, level, durability, cost):
 		self.name = name
 		self.damage = damage
 		self.swing_speed = swing_speed
 		self.level = level
 		self.durability = durability
-		self.max_durability = max_durability
-		self.max_damage_buffer = max_damage_buffer
-		self.swing_buffer = swing_buffer ####Changes from maxswing to  swing_buffer
 		self.cost = cost
 
 		
-	def checklevel(self):
-		if self.level == 1:
-			self.max_durability = 100
-			self.max_damage_buffer = 10   #Changes to buffer instead of max_damage
-			self.swing_buffer = 5
-		if self.level == 2:
-			self.max_durability = 120
-			self.max_damage_buffer = 15
-			self.swing_buffer = 6
-		if self.level == 3:
+	def getConfigs(self):
+	  ### Iron Longsword configs
+		if self.level == 1 and self.name == "Iron Longsword":
+			self.durability = 100
+			self.damage = 1   #Changes to buffer instead of max_damage
+		if self.level == 2 and self.name == "Iron Longsword":
+			self.durability = 120
+			self.damage = 3
+		if self.level == 3 and self.name == "Iron Longsword":
 			self.max_durability = 140
-			self.max_damage_buffer = 20
-			self.swing_buffer = 7
-		if self.level == 4:
-			self.max_durability = 160
-			self.max_damage_buffer = 25
-			self.swing_buffer = 8
-		if self.level == 5:
-			self.max_durability = 180
-			self.max_damage_buffer = 30
-			self.swing_buffer = 9
+			self.damage = 7
+		if self.level == 4 and self.name == "Iron Longsword":
+			self.durability = 160
+			self.damage = 15
+		if self.level == 5 and self.name == "Iron Longsword":
+			self.durability = 180
+			self.damage = 35
 
 
 
