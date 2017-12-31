6d8cfece69d0b4cb2f3b0342d97d9b2223cfcd0c
Landen Brown <31902019+LandenBrown@users.noreply.github.com>
2017-12-31 14:08:57 +0300

    Less objects to initialize

--- Assets/Engine_Parts.py
+++ Assets/Engine_Parts.py
@@ -18,20 +18,9 @@ def Engine_Start_Init():
         #####CRATHER CASTLE####
         crathercastle.tavern = crathertavern
         crathercastle.shop = crathershop
-        ######CRATHER SHOP
-        crathershop.items = [iron_knife, iron_longsword, iron_greatsword]
         crathercastle.sub1 = cratherdungeon
-        ######CRATHER DUNGEON
-        cratherdungeon.resource = iron
-        cratherdungeon.monster_rating = 1
         crathercastle.sub2 = cratherplains
-        #####CRATHER PLAINS
-        cratherplains.resource = marigold
-        cratherplains.monster_rating = 2
         crathercastle.sub3 = cratherruins
-        #####CRATHER RUINS
-        cratherruins.resource = copper
-        cratherruins.monster_rating = 4
         ############################################################################
         ################################---DEFINING MONSTER VALUES---####################
         ############################################################################
@@ -46,24 +35,7 @@ def Engine_Start_Init():
         ############################################################################
 
 
-##class Sword:
-##        def __init__(self, damage, cost):
-##                self.damage = damage
-##                self.cost = cost
-##
-##
-##def Get_Drop_Config(m):
-##        print "placeholder"
-##        
-##        
-##
-######MIND FUCKING BLOWNNNNNNN
-##def Generate_Weapon(d, c):
-##        neweapon = Sword(d, c)
-##        return neweapon
 
-###BRAIN STORMING
-###player.weapon = Generate_Weapon(d, c)
 
 ##arrayof array // this Type of monster drops these
 # monster loot drop [[], [], []]  
